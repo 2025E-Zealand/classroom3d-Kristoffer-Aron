4f478c7da79e59e1d3ee0161d69dcdb322da83b3
Kristoffer Aron Hendriksson <krh004@edu.zealand.dk>
2025-08-27 11:35:06 +0200

    Student Created

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Student
+    {
+        private string Name { get; set; }
+        private int BirthMonth { get; set; }
+        private int Birthday { get; set; }
+
+        public Student(string name, int birthmonth, int birthday)
+        {
+            Name = name;
+            BirthMonth = birthmonth;
+            Birthday = birthday;
+        }
+    }
+}
